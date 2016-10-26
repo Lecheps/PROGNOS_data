148b9b55bef19cc288a022d05786772e37b92b00
jose-luis <jlg@niva.no>
2016-10-26 16:30:21 +0200

    First commit

---
+++ AquaMonitor.py
@@ -0,0 +1,118 @@
+__author__ = 'rbr'
+
+import urllib
+import urllib2
+from xml.dom import minidom
+import json
+
+host = 'http://www.aquamonitor.no/'
+#host = 'http://localhost/'
+#host = 'http://151.157.129.195/'
+
+
+class RequestWithMethod(urllib2.Request):
+   def __init__(self, method, *args, **kwargs):
+    self._method = method
+    urllib2.Request.__init__(self, *args, **kwargs)
+
+   def get_method(self):
+    return self._method
+
+
+def requestService(url, params):
+
+    req = urllib2.Request(url, urllib.urlencode(params))
+    response = urllib2.urlopen(req)
+
+    return minidom.parseString(response.read())
+
+
+def login(site, username, password):
+
+    loginurl =  host + site + '/WebServices/LoginService.asmx/AuthenticateUser'
+    loginparams = {'username':username, 'password':password}
+
+    userdom = requestService(loginurl, loginparams)
+
+    usertype = userdom.getElementsByTagName('Usertype')[0].childNodes[0].nodeValue
+
+    if not usertype == 'NoUser':
+        token = userdom.getElementsByTagName('Token')[0].childNodes[0].nodeValue
+    else:
+        token = None
+
+    return token
+
+
+def getJson(token, path):
+    header = {'Cookie':'aqua_key='+token+';'}
+    req = urllib2.Request(host + path, None, header)
+    response = urllib2.urlopen(req)
+    return json.loads(response.read())
+
+def postJson(token, path, inJson):
+
+    header = {'Content-Type':'application/json','Cookie':'aqua_key='+token+';'}
+    req = urllib2.Request(host+path, json.dumps(inJson), header)
+    response = urllib2.urlopen(req)
+    return json.loads(response.read())
+
+def putJson(token, path, inJson):
+
+    header = {'Content-Type':'application/json','Cookie':'aqua_key='+token+';'}
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+
+    req = urllib2.Request(host+path, json.dumps(inJson), header)
+    req.get_method = lambda: "PUT"
+    response = opener.open(req)
+    return json.loads(response.read())
+
+
+def getProject(token, projectId):
+
+    projectsurl = 'AquaServices/api/projects/'+ str(projectId)
+    return getJson(token, projectsurl)
+
+def getStations(token, projectId):
+
+    stationsurl = 'AquaServices/api/projects/' + str(projectId) + '/stations/'
+    return getJson(token, stationsurl)
+
+def getArchive(token, id):
+    header = {'Content-Type': 'application/json', 'Cookie':'aqua_key='+token+';'}
+    archiveurl = host + 'AquaServices/files/archive/' + id
+    req = urllib2.Request(archiveurl, None, headers=header)
+    response = urllib2.urlopen(req)
+    return json.loads(response.read())
+
+
+def createDatafile(token, data):
+
+    header = {'Content-Type': 'application/json', 'Cookie':'aqua_key='+token+';'}
+    datafileurl = host + 'AquaServices/files/datafile/'
+    req = urllib2.Request(datafileurl, json.dumps(data), header)
+    response = urllib2.urlopen(req)
+    return json.loads(response.read())
+
+def deleteArchive(token, id):
+    header = {'Content-Type': 'application/json', 'Cookie':'aqua_key='+token+';'}
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+
+    archiveurl = host + 'AquaServices/files/archive/' + id
+    req = urllib2.Request(archiveurl, None, headers=header)
+    req.get_method = lambda: "DELETE"
+    response = opener.open(req)
+
+    return response.read()
+
+def download(token, id, file, path):
+    output = open(path, 'wb')
+
+    url = host + "AquaServices/files/archive/" + id + '/' + file
+
+    headers = {'Cookie' : 'aqua_key=' + token + ';'}
+
+    req = urllib2.Request(url, None, headers)
+    resp = urllib2.urlopen(req)
+    output.write(resp.read())
+    output.close()
---
+++ LangtjernWeather.py
@@ -0,0 +1,75 @@
+import AquaMonitor
+import datetime
+import pandas as pd
+#import numpy as np
+
+
+
+def getLangtjernData(username,password,root,fromDate,toDate) :
+    weatherFile = 'langtjern_weather.csv'
+    lakeFile = 'langtjern_lake.csv'
+    outletFile = 'langtjern_outlet.csv'
+    inletFile = 'langtjern_inlet.csv'
+
+    fromDate = datetime.datetime.strptime(fromDate,"%Y/%m/%d").date()
+    toDate = datetime.datetime.strptime(toDate,"%Y/%m/%d").date()
+    expires = datetime.date.today() + datetime.timedelta( days=1 )
+    
+    token = AquaMonitor.login("AquaServices", username, password)
+    
+    def make_file(title, filename, stationid, datatype) :
+        where = "sample_date>=" + datetime.datetime.strftime(fromDate, '%d.%m.%Y') \
+            + " and sample_date<" + datetime.datetime.strftime(toDate, '%d.%m.%Y') \
+            + " and datatype=" + datatype
+        data = {
+            "Expires": expires.strftime('%Y.%m.%d'),
+            "Title": title,
+            "Files":[{
+                "Filename": filename,
+                "ContentType":"text/csv"}],
+            "Definition":{
+                "Format":"csv",
+                "StationIds": [ stationid ],
+                "DataWhere": where
+            }
+        }
+        resp = AquaMonitor.createDatafile(token, data)
+        return resp["Id"]
+    
+    
+    def download_file(id, filename) :
+        archived = False
+        while not archived:
+            resp = AquaMonitor.getArchive(token, id)
+            archived = resp["Archived"]
+        path = root + filename
+        AquaMonitor.download(token, id, filename, path)
+
+
+    
+    #Four measurement id's can be obtained from Langtjern
+    weatherFileId = make_file('Langtjern weather', weatherFile, 62040, 'Air')
+    lakeFileId = make_file('Langtjern lake', lakeFile, 50472, 'Water')
+    outletFileId = make_file('Langtjern outlet', outletFile, 51356, 'Water')
+    inletFileId = make_file('Langtjern inlet', inletFile, 63098, 'Water')
+    
+    download_file(weatherFileId, weatherFile)
+    download_file(lakeFileId, lakeFile)
+    download_file(inletFileId, inletFile)
+    download_file(outletFileId, outletFile)
+    
+    allData = {}
+    
+    #Putting the data into the right format
+    #First placing dataframes in a dictionary
+    allData['weather'] = pd.read_csv(root + weatherFile,    delimiter=';', encoding='utf-16')
+    allData['lake']    = pd.read_csv(root + lakeFile,       delimiter=';', encoding='utf-16')
+    allData['outlet']  = pd.read_csv(root + outletFile,     delimiter=';', encoding='utf-16')
+    allData['inlet']   = pd.read_csv(root + inletFile,      delimiter=';', encoding='utf-16')
+    
+    #Make the index of the dataframes a timestamp, storing only float data (hopefully measurements)
+    for key, i in allData.iteritems():
+        i.set_index(pd.to_datetime(i["SampleDate_dato"].map(str) + ' ' + i["SampleDate_tid"],format = '%d.%m.%Y %H:%M:%S',utc=True),inplace=True)
+        allData[key] = i.loc[:,i.dtypes == 'float64']
+    return allData
+    
